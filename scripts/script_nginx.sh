9b703f2fb41108b40f5feb59510683cf561ffd51
rnav413 <69471412+rnav413@users.noreply.github.com>
2024-05-20 01:44:38 -0500

    Delete scripts/script_nginx.sh

--- scripts/script_nginx.sh
+++
@@ -1,7 +0,0 @@
-echo "------------- ngix -------------"
-sudo apt update
-sudo apt install -y nginx
-
-
-echo "------------- comprovando si nginx fue instalado -------------"
-sudo systemctl status nginx
